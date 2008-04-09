9e918c1df56747aeda64e85395fbe0d64fdd9591
Tore Darell <toredarell@gmail.com>
2008-04-09 21:46:26 +0200

    Update demo

---
+++ demo/comment_form.js
@@ -0,0 +1,34 @@
+Demo.CommentForm = ActiveElement.Form.spawn('comment', {
+
+  extend: {
+    findInDocument: function(){
+      return new this($$('.edit_comment', '.new_comment').first());
+    },
+    attach: function(form){
+      Demo.commentForm = form;
+    }
+  },
+
+  afterInitialize: function(){
+    this.element.observe('submit', function(e){
+      e.stop();
+      alert(
+        this.getAttributeNames().map(function(n){
+          return n+': '+this.get(n);
+        }, this).join('\n')+
+        "\n\nNot actually sending this, because there's no server on the other end"
+      );
+    }.bindAsEventListener(this));
+    console.log(
+      'Comment form initialised ('+
+      this.getAttributeNames().map(function(a){ return '"'+a+'"'; }).join(', ')+')'
+    );
+  },
+
+  loudify: function(){
+    this.getAttributeNames().each(function(name){
+      this.set(name, this.get(name).toUpperCase());
+    }, this);
+  }
+
+});
